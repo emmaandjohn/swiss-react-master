6ef0dd88a1d4bc90217ec27c20e5e9772c337056
Janina Lorenz <janinalorenz@Janinas-MacBook-Air.local>
2016-10-31 21:19:35 -0500

    mexiko v1.9

--- src/server.js
+++ src/server.js
@@ -138,12 +138,27 @@ app.post('/login', function(req, res) {
 app.post('/forgot', function(req, res) {
 
     var email = req.body.email;
+    var getPw = '';
 
     UserModel.findOne({ email: email, activation: true }, 'password', function(error, result){
         if(error){
             res.json(error);
         }
         else if(result !== null){
+            getPw = pw.password;
+            var mailOptions = {
+                from: '"Swiss React Community: Passwort vergessen" <info@swiss-react.ch>',
+                to: email,
+                subject: 'Passwort erneut zugestellt',
+                text: 'Passwort vergessen',
+                html: '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"><html xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8"/><title>Willkommen bei Swiss-React.ch</title></head><body bgcolor="#8d8e90"><table width="100%" border="0" cellspacing="0" cellpadding="0" bgcolor="#8d8e90"> <tr> <td><table width="600" border="0" cellspacing="0" cellpadding="0" bgcolor="#FFFFFF" align="center"> <tr> <td><table width="100%" border="0" cellspacing="0" cellpadding="0"> <tr> <td width="61"><a href="//www.swiss-react.ch" target="_blank"><img src="https://s3.eu-central-1.amazonaws.com/swiss-react.ch/resources/img/PROMO-GREEN2_01_01.jpg" width="61" height="76" border="0" alt=""/></a></td><td width="144"><a href="//www.swiss-react.ch" target="_blank"><img src="https://s3.eu-central-1.amazonaws.com/swiss-react.ch/resources/img/PROMO-GREEN2_01_02.jpg" width="144" height="76" border="0" alt=""/></a></td><td width="393"><table width="100%" border="0" cellspacing="0" cellpadding="0"> <tr> <td height="30"><img src="https://s3.eu-central-1.amazonaws.com/swiss-react.ch/resources/img/PROMO-GREEN2_01_04.jpg" width="393" height="30" border="0" alt=""/></td></tr></table></td></tr></table></td></tr><tr> <td align="center">&nbsp;</td></tr><tr> <td>&nbsp;</td></tr><tr> <td><table width="100%" border="0" cellspacing="0" cellpadding="0"> <tr> <td width="10%">&nbsp;</td><td width="80%" align="left" valign="top"><font style="font-family: Georgia, Times, serif; color:#010101; font-size:24px"><strong><em>Passwort vergessen</em></strong></font><br/><br/> <font style="font-family: Verdana, Geneva, sans-serif; color:#666766; font-size:13px; line-height:21px">Das Passwort für deinen Swiss-React.ch Account:<br/><br/><strong>'+getPw+'</strong><br/><br/>Solltest du kein Passwort von <a href="http://swiss-react.ch">swiss-react.ch</a> angefordert haben, bitten wir dich diese Email zu ignorieren.<br/>Dein Swiss React Team</font></td><td width="10%">&nbsp;</td></tr></table></td></tr><tr> <td>&nbsp;</td></tr><tr> <td>&nbsp;</td></tr><tr> <td><img src="https://s3.eu-central-1.amazonaws.com/swiss-react.ch/resources/img/PROMO-GREEN2_07.jpg" width="598" height="7" style="display:block" border="0" alt=""/></td></tr><tr> <td>&nbsp;</td></tr><tr> <td align="center"><font style="font-family: Helvetica, Arial, sans-serif; color:#231f20; font-size:8px"><strong>Swiss React Community by Emma &amp; John | www.emmaandjohn.ch </strong></font></td></tr><tr> <td>&nbsp;</td></tr></table></td></tr></table></body></html>'
+            };
+            transporter.sendMail(mailOptions, function(error, info){
+                if(error){
+                    return console.log(error);
+                }
+                console.log('Message sent: ' + info.response);
+            });
             res.json({ status: 1, pw: result });
         }
         else{
