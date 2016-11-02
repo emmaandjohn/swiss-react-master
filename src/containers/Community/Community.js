95860af5e4ea2228760a2c066ecc51ac22e0f48e
Janina Lorenz <janinalorenz@Janinas-MacBook-Air.local>
2016-11-02 10:57:53 -0500

    mexiko v5.2

--- src/containers/Community/Community.js
+++ src/containers/Community/Community.js
@@ -251,12 +251,12 @@ export default class RichEditorExample extends Component {
  }
 
  const BLOCK_TYPES = [
-   {label: 'H1', style: 'header-one'},
+   /*{label: 'H1', style: 'header-one'},
    {label: 'H2', style: 'header-two'},
-   {label: 'H3', style: 'header-three'},
-   {label: 'H4', style: 'header-four'},
-   {label: 'H5', style: 'header-five'},
-   {label: 'H6', style: 'header-six'},
+   {label: 'H3', style: 'header-three'},*/
+   {label: 'Titel', style: 'header-four'},
+   /*{label: 'H5', style: 'header-five'},
+   {label: 'H6', style: 'header-six'},*/
    {label: 'Blockquote', style: 'blockquote'},
    {label: 'UL', style: 'unordered-list-item'},
    {label: 'OL', style: 'ordered-list-item'},
