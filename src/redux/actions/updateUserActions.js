49f2b925781c9c2dbd749ab6542e76d420a69778
Janina Lorenz <janinalorenz@Janinas-MacBook-Air.local>
2016-11-07 20:02:53 -0500

    mexiko v11.8

--- src/redux/actions/updateUserActions.js
+++ src/redux/actions/updateUserActions.js
@@ -1,5 +1,6 @@
 export function updateUser(whichField, newValue){
   var avatar = '';
+  console.log("Action whichfield OK: "+ whichField + newValue);
   if(whichField === 'avatar'){
     avatar = newValue;
   }
@@ -7,21 +8,21 @@ export function updateUser(whichField, newValue){
     dispatch({
       type: 'UPDATE_USER_FULFILLED',
       payload: {
-        pw: pw,
-        birthday: birthday,
-        avatar: avatar,
-        nickname: nickname,
-        job: job,
-        company: company,
-        description: description,
-        membersince: membersince,
-        kanton: kanton,
-        socialFb: socialFb,
-        socialGithub: socialGithub,
-        socialTwitter: socialTwitter,
-        socialLinkedin: socialLinkedin,
-        socialXing: socialXing,
-        socialWebsite: socialWebsite
+        pw: pw ? pw : undefined,
+        birthday: birthday ? birthday : undefined,
+        avatar: avatar ? avatar : undefined,
+        nickname: nickname ? nickname : undefined,
+        job: job ? job : undefined,
+        company: company ? company : undefined,
+        description: description ? description : undefined,
+        membersince: membersince ? membersince : undefined,
+        kanton: kanton ? kanton : undefined,
+        socialFb: socialFb ? socialFb : undefined,
+        socialGithub: socialGithub ? socialGithub : undefined,
+        socialTwitter: socialTwitter ? socialTwitter : undefined,
+        socialLinkedin: socialLinkedin ? socialLinkedin : undefined,
+        socialXing: socialXing ? socialXing : undefined,
+        socialWebsite: socialWebsite ? socialWebsite : undefined
       }
     });
   };
