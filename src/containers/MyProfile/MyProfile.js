aaa7ac2becd0fc175303c8f5efea3e7de7deaa4b
Janina Lorenz <janinalorenz@Janinas-MacBook-Air.local>
2016-11-09 16:20:16 -0500

    mexiko v17.1

--- src/containers/MyProfile/MyProfile.js
+++ src/containers/MyProfile/MyProfile.js
@@ -156,7 +156,7 @@ export default class MyProfile extends Component {
                 </form>
               </div>
               }
-              <hr />
+
               <Row>
                 <Col xs={12} sm={6}>
                   <Row>
