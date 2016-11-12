aed67f8b64615a61935e5173a5213490bd4c4925
Janina Lorenz <janinalorenz@Janinas-MacBook-Air.local>
2016-11-11 23:17:50 -0500

    mexiko last 9

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -49,7 +49,7 @@ export default class Home extends Component {
                         entry.userEmail + ' | ' +
                         entry.timeFormatted + '</span></div>';
                         */
-      blogContentDef += '<div class="col-xs-1">'+ '<div class="' + stylesMyProfile['avatar'+entry.userAvatar] + ' ' + stylesMyProfile.avatarRound + ' ' + stylesMyProfile.avatarMain + ' ' + stylesMyProfile.avatarMini'"></div></div>' +
+      blogContentDef += '<hr /><div class="col-xs-1"><div class="' + stylesMyProfile['avatar'+entry.userAvatar] + ' ' + stylesMyProfile.avatarRound + ' ' + stylesMyProfile.avatarMain + ' ' + stylesMyProfile.avatarMini'"></div></div>' +
                         '<div class="col-xs-3">'+ entry.userNickname + '</div>' +
                         '<div class="col-xs-3">'+ entry.titel + '</div>' +
                         '<div class="col-xs-1">'+ entry.category + '</div>' +
@@ -75,7 +75,7 @@ export default class Home extends Component {
 
         <div className='container'>
           <h3>React Activity Feed</h3>
-            <div className={styles.topLine + ' row'} dangerouslySetInnerHTML={{__html: blogContentDef}}></div>
+            <div className='row' dangerouslySetInnerHTML={{__html: blogContentDef}}></div>
         </div>
       </div>
     );
