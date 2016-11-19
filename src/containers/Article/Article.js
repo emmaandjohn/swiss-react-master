b8f0dae9a8660ef87bba3964eb1688f12ee03e1a
Pascal Scheidegger <paschcua@hispeed.ch>
2016-11-20 00:58:44 +0100

    sda5

--- src/containers/Article/Article.js
+++ src/containers/Article/Article.js
@@ -22,8 +22,6 @@ export default class Article extends Component {
   }
 
   componentDidMount() {
-    console.log("Artid1: "+this.props.params.id);
-    console.log("Artid2: "+this.props.location.query.id);
     let getUrl = this.props.params.id;
 
     superagent
@@ -47,38 +45,23 @@ export default class Article extends Component {
     const { activateNewUserState, getBlogEntriesState } = this.props;
 
     console.log("specificArticleData: "+JSON.stringify(specificArticleData));
-    //console.log("getBlogEntriesState: "+JSON.stringify(getBlogEntriesState.articles.userNickname));
-
-    /*let blogContentDef = [];
-    getBlogEntriesState.articles.forEach(function(entry){
-      blogContentDef.push(
-        <div className={styles.topLine + ' col-xs-12'}>
-          <div className='col-xs-1'>
-            <div className={stylesMyProfile['avatar'+entry.userAvatar] + ' ' + stylesMyProfile.avatarRound + ' ' + stylesMyProfile.avatarMain + ' ' + stylesMyProfile.avatarMini}></div>
-          </div>
-          <div className='col-xs-2'>{entry.userNickname}</div>
-          <div className='col-xs-3'><strong>{entry.titel}</strong></div>
-          <div className='col-xs-1'><span className="label label-primary">{entry.category}</span></div>
-          <div className={'col-xs-3 ' + styles.techStyle}>{entry.technologies}</div>
-          <div className={'col-xs-2 ' + styles.dateStyle}>{entry.timeFormatted}</div>
-          <div className={'col-xs-12'}>{entry.markup}</div>
-        </div>
-      );
-    }.bind(this));*/
-
-    //let blogContentDef = '';
-    // Todo: make it JSX style not like this.. see Home.js for sample.
-    // And of course get here specific Article from the blog-ID in updated STATE from Home.js
-    /*getBlogEntriesState.articles.forEach(function(entry){
-      blogContentDef += '<div style="background-color: #FDFDFD; border: 1px dotted #C8C8C8; padding: 12px; margin: 30px auto;"><h2 style="color: #d52b1e;">' + entry.titel + '</h2><hr />' + entry.markup + '<br><span style="font-size: 10px; font-style: italic; color: grey;">Author: ' + entry.userNickname + ' | ' + entry.timeFormatted + '</span></div>';
-    });*/
 
     return (
       <div className="container" id="articlePage">
         <Helmet title="Article"/>
         {(activateNewUserState.activatedUser === true && activateNewUserState.loggedInUser === true) || (cookie.load('ck_userLoggedIn') === true && cookie.load('ck_activation') === true) ?
           <div className='row'>
-          asd
+            <div className={styles.topLine + ' col-xs-12'}>
+              <div className='col-xs-1'>
+                <div className={stylesMyProfile['avatar'+specificArticleData.userAvatar] + ' ' + stylesMyProfile.avatarRound + ' ' + stylesMyProfile.avatarMain + ' ' + stylesMyProfile.avatarMini}></div>
+              </div>
+              <div className='col-xs-2'>{specificArticleData.userNickname}</div>
+              <div className='col-xs-3'><strong>{specificArticleData.titel}</strong></div>
+              <div className='col-xs-1'><span className="label label-primary">{specificArticleData.category}</span></div>
+              <div className={'col-xs-3 ' + styles.techStyle}>{specificArticleData.technologies}</div>
+              <div className={'col-xs-2 ' + styles.dateStyle}>{specificArticleData.timeFormatted}</div>
+              <div className={'col-xs-12'}>{specificArticleData.markup}</div>
+            </div>
           </div>
         : null
         }
