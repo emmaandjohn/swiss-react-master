50a79ad773176f251bbd99913a0da3cd66fd5f25
Pascal Scheidegger <paschcua@hispeed.ch>
2016-11-29 18:07:55 +0100

    testv17.2

--- src/containers/Community/Community.js
+++ src/containers/Community/Community.js
@@ -114,7 +114,9 @@ export default class RichEditorExample extends Component {
   }
 
   onChangeCheckbox = (event, t, tValue) => {
-    this.setState({ techObject[t]: event.target.checked });
+    this.state.techObject.[t] = event.target.checked;
+    this.forceUpdate();
+    //this.setState({ techObject[t]: event.target.checked });
     console.log(JSON.stringify(this.state.techObject));
   }
 
