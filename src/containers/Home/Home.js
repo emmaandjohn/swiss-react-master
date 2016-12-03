3620f80f14764fbe83a7da84ee4ca24d9698c204
Janina Lorenz <janinalorenz@Janinas-MacBook-Air.local>
2016-12-04 00:02:40 +0100

    scuol v3.5

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -57,6 +57,7 @@ export default class Home extends Component {
     getBlogEntriesState.articles.forEach(function(entry){
       console.log(JSON.stringify(entry));
       blogContentDef.push(
+        const techVals = Object.keys(entry.technologies[0]).map(key => entry.technologies[0][key]);
         <div onClick={() => this.loadArticle(entry.articleId)} className={styles.topLine + ' col-xs-12 ' + styles.hover}>
           <div className='row'>
             <div className='col-sm-1 col-xs-6'>
@@ -65,7 +66,7 @@ export default class Home extends Component {
             <div className='col-sm-2 col-xs-6'>{entry.userNickname}</div>
             <div className='col-sm-3 col-xs-12'><strong>{entry.titel}</strong></div>
             <div className='col-sm-1 col-xs-12'><span className="label label-primary">{entry.category}</span></div>
-            <div className={'col-sm-3 col-xs-12 ' + styles.techStyle}>{entry.technologies.t01}</div>
+            <div className={'col-sm-3 col-xs-12 ' + styles.techStyle}>{techVals}</div>
             <div className={'col-sm-2 col-xs-12 ' + styles.dateStyle}>{entry.timeFormatted}</div>
           </div>
         </div>
