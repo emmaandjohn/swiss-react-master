ef3635003957bfe846190227dc2ae279a7b97780
Pascal Scheidegger <paschcua@hispeed.ch>
2016-12-11 15:48:00 +0100

    aha16.1

--- src/containers/Article/Article.js
+++ src/containers/Article/Article.js
@@ -18,7 +18,8 @@ export default class Article extends Component {
   state = {
     formStatus: 0,
     formMsg: '',
-    specificArticleData: {}
+    specificArticleData: {},
+    specificArticleTechData: ''
   }
 
   componentDidMount() {
@@ -31,7 +32,9 @@ export default class Article extends Component {
     .end((error, res) => {
       if(res.body.status === 1) {
         this.setState({specificArticleData: res.body.specificArticleData});
-        console.log(this.state.specificArticleData.technologies[0]);
+        this.setState({ specificArticleTechData: Object.keys(this.state.specificArticleData.technologies[0]).map(
+          key => this.state.specificArticleData.technologies[0][key]
+        ) });
       } else{
         console.log("Error, Article url does not exist in DB");
       }
@@ -42,7 +45,7 @@ export default class Article extends Component {
     const styles = require('../Home/Home.scss');
     const stylesMyProfile = require('../MyProfile/MyProfile.scss');
 
-    const {formStatus, formMsg, specificArticleData} = this.state;
+    const {formStatus, formMsg, specificArticleData, specificArticleTechData} = this.state;
     const { activateNewUserState, getBlogEntriesState} = this.props;
 
     /*
@@ -81,7 +84,7 @@ export default class Article extends Component {
                 </div>
                 <div className='col-sm-9 col-xs-6'>{specificArticleData.userNickname}</div>
                 <div className='col-sm-1 col-xs-12'><span className="label label-primary">{specificArticleData.category}</span></div>
-                <div className={'col-sm-3 col-xs-12' + styles.techStyle}>{ "Object.keys(specificArticleData.technologies[0]).map(key => specificArticleData.technologies[0][key])" }</div>
+                <div className={'col-sm-3 col-xs-12' + styles.techStyle}>{specificArticleTechData}</div>
                 <div className={'col-sm-8 col-xs-12' + styles.dateStyle}>{specificArticleData.timeFormatted}</div>
                 <div className={'col-sm-12 col-xs-12'}><div dangerouslySetInnerHTML={{__html: specificArticleData.markup}}></div></div>
               </div>
