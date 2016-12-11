0354cb3630dd1db93efe55a038a35e3530623354
Pascal Scheidegger <paschcua@hispeed.ch>
2016-12-11 14:48:31 +0100

    aha8

--- src/containers/Article/Article.js
+++ src/containers/Article/Article.js
@@ -44,7 +44,7 @@ export default class Article extends Component {
     const {formStatus, formMsg, specificArticleData} = this.state;
     const { activateNewUserState, getBlogEntriesState } = this.props;
 
-    console.log( Object.keys(specificArticleData.technologies[0]).map(key => specificArticleData.technologies[0][key].bind.this(); ) );
+    console.log( Object.keys(specificArticleData.technologies[0].bind.this()).map(key => specificArticleData.technologies[0][key]));
 
     return (
       <div className="container" id="articlePage">
