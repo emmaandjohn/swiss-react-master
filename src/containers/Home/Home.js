da8fad9906fd11f734d000b33d3de01cdc99739f
Pascal Scheidegger <paschcua@hispeed.ch>
2016-12-20 15:36:53 +0100

    safd2sss5sss4sssssss

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -60,6 +60,7 @@ export default class Home extends Component {
       }else{
         whichCategory = <span className="label label-info">{entry.category}</span>;
       }
+      console.log(JSON.stringify(whichCategory));
       blogContentDef.push(
         <div onClick={() => this.loadArticle(entry.articleId)} className={styles.topLine + ' col-xs-12 ' + styles.hover}>
           <div className='row'>
@@ -69,7 +70,7 @@ export default class Home extends Component {
             </div>
             <div className='col-sm-2 col-xs-6'>{entry.userNickname}</div>
             <div className='col-sm-3 col-xs-12'><strong>{entry.titel}</strong></div>
-            <div className='col-sm-1 col-xs-12'><div dangerouslySetInnerHTML={{__html: whichCategory}}></div></div>
+            <div className='col-sm-1 col-xs-12'>{whichCategory}</div>
             <div className={'col-sm-3 col-xs-12 ' + styles.techStyle}>{ Object.keys(entry.technologies[0]).map(key => entry.technologies[0][key]) }</div>
             <div className={'col-sm-2 col-xs-12 ' + styles.dateStyle}>{entry.timeFormatted}</div>
           </div>
