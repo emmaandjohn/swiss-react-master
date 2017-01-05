2427e33e199e8322a55d8cb9f6e248f583c0d40b
Pascal Scheidegger <paschcua@hispeed.ch>
2017-01-05 22:39:10 +0100

    mmmmm33.122

--- src/containers/MyProfile/MyProfile.js
+++ src/containers/MyProfile/MyProfile.js
@@ -311,7 +311,7 @@ export default class MyProfile extends Component {
               <div className='col-xs-2 col-sm-1'>
                 <div className={avatarClass + ' ' + styles.avatarRound + ' ' + styles.avatarMain + ' ' + styles.avatarMiniProfile}></div>
               </div>
-              <div className='col-xs-10 col-sm-11'>
+              <div className={'col-xs-10 col-sm-11' + ' ' + styles.wellLayouting}>
                 <div className='col-xs-12'>{entry.titel + ' ' + styles.m5}</div>
                 <div className={'col-xs-12 ' + stylesHome.dateStyle + ' ' + styles.m5}>{entry.timeFormatted}</div>
                 <div className={'col-xs-12 ' + stylesHome.techStyle}>{ Object.keys(entry.technologies[0]).map(key => entry.technologies[0][key].length > 1 ? <span title={entry.technologies[0][key]} className={stylesCommunity.cbs00Home + ' ' + stylesCommunity['cbs'+key]}></span> : null ) }</div>
@@ -526,7 +526,7 @@ export default class MyProfile extends Component {
                     </Col>
 
                     <Col className={styles.m15 + ' ' + styles.topLine + ' ' + styles.font999} xs={12}>Über dich <Button bsSize="small" className={styles.btnEdit} onClick={() => this.show4a(false)}><i className="fa fa-pencil"/></Button></Col>
-                    <Col className={styles.m15 + ' ' + styles.pb20border + ' ' + styles.topLine2} xs={12}>
+                    <Col className={styles.m15 + ' ' + styles.pb20border} xs={12}>
                       {this.state.show4a === true ?
                         <div className={styles.makeItalic + ' ' + styles.whiteSpacePreWrap}>{getDescription}</div>
                       :
