026aa5ed6da4007912d37cce831ccdd70d22f323
Pascal Scheidegger <paschcua@hispeed.ch>
2017-01-08 23:44:14 +0100

    asdee1222qa2

--- src/server.js
+++ src/server.js
@@ -287,9 +287,11 @@ app.post('/searchQuery', function(req, res) {
         var techFilteredObject;
         for (var i = 0; i < result.length; i++) {
           for (const key of Object.keys(result[i].technologies[0])) {
-
-            if(result[i].technologies[0][key].length > 1){
-              console.log('Walking east one step: ' + JSON.stringify(result[i]));
+              console.log("key: "+key + ", key2: " + result[i].technologies[0].key) + ", i:"+i;
+            if(result[i].technologies[0].key.length > 1){
+              if(newarr.indexOf(result[i].technologies[0].key) > -1){
+                console.log('Walking east one step: ' + JSON.stringify(result[i]));
+              }
             }
           }
         }
