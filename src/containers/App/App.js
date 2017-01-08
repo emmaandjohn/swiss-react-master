1e493695705240a30f4025007b469bc14c1824c7
Pascal Scheidegger <paschcua@hispeed.ch>
2017-01-08 13:11:29 +0100

    asdee fix2

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -129,18 +129,18 @@ export default class App extends Component {
                     </LinkContainer>
                     {(activateNewUserState.activatedUser === true && activateNewUserState.loggedInUser === true) || (cookie.load('ck_userLoggedIn') === 'true' && cookie.load('ck_activation') === 'true') ?
                     <LinkContainer to="/meinprofil">
-                      <NavItem eventKey={3} onClick={ this.onNavItemClick }><i className={"fa fa-user" + styles.faNav} aria-hidden="true"></i> Mein Profil</NavItem>
+                      <NavItem eventKey={3} onClick={ this.onNavItemClick }><i className={"fa fa-user " + styles.faNav} aria-hidden="true"></i> Mein Profil</NavItem>
                     </LinkContainer>
                     :
                     <LinkContainer to="/registrieren">
-                      <NavItem eventKey={3} onClick={ this.onNavItemClick }>Mitmachen</NavItem>
+                      <NavItem eventKey={3} onClick={ this.onNavItemClick }><i className={"fa fa-sign-in " + styles.faNav} aria-hidden="true"></i> Mitmachen</NavItem>
                     </LinkContainer>
                     }
                     {(activateNewUserState.activatedUser === true && activateNewUserState.loggedInUser === true) || (cookie.load('ck_userLoggedIn') === 'true' && cookie.load('ck_activation') === 'true') ?
-                      <NavItem eventKey={4} onClick={ this.onLogout }><i className={"fa fa-sign-out" + styles.faNav} aria-hidden="true"></i> Logout</NavItem>
+                      <NavItem eventKey={4} onClick={ this.onLogout }><i className={"fa fa-sign-out " + styles.faNav} aria-hidden="true"></i> Logout</NavItem>
                     :
                     <LinkContainer to="/login">
-                      <NavItem eventKey={5} onClick={ this.onNavItemClick }><i className={"fa fa-sign-in" + styles.faNav} aria-hidden="true"></i> Login</NavItem>
+                      <NavItem eventKey={5} onClick={ this.onNavItemClick }><i className={"fa fa-sign-in " + styles.faNav} aria-hidden="true"></i> Login</NavItem>
                     </LinkContainer>
                     }
                 </Nav>
