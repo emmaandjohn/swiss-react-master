e51d74a2dc51c0328faf8bb74ea6f7bae3c84c45
Pascal Scheidegger <pascal.scheidegger@ringier.ch>
2017-02-05 14:56:04 +0100

    ddssds

--- src/server.js
+++ src/server.js
@@ -669,7 +669,7 @@ app.post('/community', function(req, res) {
 
 /* **** Get newsest Users (Home) */
 app.post('/getNewestUsers', function(req, res) {
-      UserModel.find({ activation: true }).limit(30).sort({date: -1}).exec(function(err, result) {
+      UserModel.find({ activation: true }).limit(30).sort({'membersinceU': -1}).exec(function(err, result) {
         if(err){
           res.json(err);
           res.json({ status: 0 });
